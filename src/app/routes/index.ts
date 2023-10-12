f1513cda5e3a4713cbb85434c26b662a3ec0e683
unknown <syednew5000@gmail.com>
2023-10-12 15:39:18 +0600

    [done] complete auth,user,category

---
+++ src/app/routes/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { AuthRoutes } from '../modules/auth/auth.route';
+import { UserRoutes } from '../modules/user/user.route';
+import { CategoryRoutes } from '../modules/category/category.route';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
+  {
+    path: '/users',
+    route: UserRoutes,
+  },
+  {
+    path: '/categories',
+    route: CategoryRoutes,
+  },
+  //   {
+  //     path: '/books',
+  //     route: BookRoutes,
+  //   },
+  //   {
+  //     path: '/orders',
+  //     route: OrderRoutes,
+  //   },
+  //   {
+  //     path: '/profile',
+  //     route: ProfileRoute,
+  //   },
+];
+
+moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
